3bd68da7e43efe72de4db1e5c718b1aa1cad3e81
joao vitor <a14658@esars.pt>
2024-10-30 09:14:22 +0000

    adição do histórico

--- calculadora.py
+++ calculadora.py
@@ -6,12 +6,21 @@ def calcular():
     print("2. Subtração")
     print("3. Multiplicação")
     print("4. Divisão")
+    print("5. Histórico")
 
     while True:
-        escolha = input("Digite o número da operação (1/2/3/4) para encerrar: ")
+        escolha = input("Digite o número da operação (1/2/3/4/5) para encerrar: ")
                 
         if escolha == '0':
             break
+        
+        elif escolha == '5':
+            if len(historico) == 0:
+                print("Histórico vazio.")
+            else:
+                print("Histórico de operações")
+                for operacao in historico:
+                    print(operacao)
 
         if escolha in ['1', '2', '3', '4']:
             num1 = float(input("Digite o primeiro número: "))
@@ -20,19 +29,23 @@ def calcular():
             if escolha == '1':
                 resultado = num1 + num2
                 print(f"{num1} + {num2} = {resultado}")
+                historico.append(f"{num1} + {num2} = {resultado}")
 
             elif escolha == '2':
                 resultado = num1 - num2
                 print(f"{num1} - {num2} = {resultado}")
+                historico.append(f"{num1} - {num2} = {resultado}")
 
             elif escolha == '3':
                 resultado = num1 * num2
                 print(f"{num1} * {num2} = {resultado}")
+                historico.append(f"{num1} * {num2} = {resultado}")
 
             elif escolha == '4':
                 if num2 != 0:
                     resultado = num1 / num2
                     print(f"{num1} / {num2} = {resultado}")
+                    historico.append(f"{num1} / {num2} = {resultado}")
                 else:
                     print("Erro: Divisão por zero não é permitida.")
 
