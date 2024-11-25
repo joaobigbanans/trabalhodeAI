af0f7d216b4969bb851288f4809f11a225eed5a2
joao vitor <a14658@esars.pt>
2024-11-25 12:29:45 +0000

    definição do dicionario

--- dicionário.py
+++ dicionário.py
@@ -6,3 +6,10 @@ stand = {
     "carro4" : {"nome": "renout zoe", "preço": 45000, "quantidade": 5},
     "carro5" : {"nome": "citroen ami", "preço": 7600, "quantidade": 11}
 }
+
+def print_stand():
+    print("Produtos disponiveis no stand:")
+    for x, y in stand.items():
+        print(f"{x}: Nome: {y["nome"]}, Preço: {y["preço"]:.2f}, Quantidae: {y["quantidade"]} unidades.")
+
+stand["carro2"] = {"nome": "porsche 911 carrera","preço": 140000,"quantidade": 4}
